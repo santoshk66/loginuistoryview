9fe1c21e64bd66e44d6943b95a48530639924c2d
santoshk66 <santosh.k66@gmail.com>
2020-10-05 17:11:41 +0530

    Add registration form

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+  private BottomNavigationView mbottom;
+  private FrameLayout mframe;
+  private First firstfragment;
+    private Second secondfragment;
+    private Third thirdfragment;
+    private four fourfragment;
+    private five fivefragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mbottom= (BottomNavigationView) findViewById(R.id.bottom_navi);
+        mframe = (FrameLayout) findViewById(R.id.fragment_container);
+
+        firstfragment = new First();
+        secondfragment = new Second();
+        thirdfragment = new Third();
+        fourfragment = new four();
+        fivefragment = new five();
+
+        setFragment(firstfragment);
+
+        mbottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.nav_first:
+                        setFragment(firstfragment);
+                        return true;
+
+                    case R.id.nav_second:
+                        setFragment(secondfragment);
+                        return true;
+
+                    case R.id.nav_third:
+                        mbottom.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(thirdfragment);
+                        return true;
+
+                    case R.id.nav_four:
+                        setFragment(fourfragment);
+                        return true;
+
+                    case R.id.nav_five:
+                        setFragment(fivefragment);
+                    return true;
+
+                    default:
+                        return false;
+                }
+            }
+
+
+        });
+    }
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/myapplication/Third.java
+++ app/src/main/java/com/example/myapplication/Third.java
@@ -1,12 +1,13 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +24,7 @@ public class Third extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public Third() {
         // Required empty public constructor
@@ -49,16 +51,23 @@ public class Third extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false);
     }
+
 }
